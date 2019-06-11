85dbcf8aa5c2f538bc07d7c7850838f59f957c26
Nate Poths <pothsn@gmail.com>
2019-06-11 13:15:58 -0500

    Laid basic structure for BinarySearchTree and Node classes.

---
+++ BinarySearchTreeProject/BinarySearchTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class BinarySearchTree
+    {
+        //Member variables
+        public Node root;
+
+
+
+        //Constructor
+        public BinarySearchTree()
+        {
+            root = new Node();
+
+
+        }
+
+        //Member methods
+        public void Add()
+        {
+
+        }
+
+        public void Search()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BinarySearchTreeProject/Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class Node
+    {
+        //Member variables
+        public int data;       
+        public Node left;
+        public Node right;
+
+        //Constructor
+        public Node()
+        {
+
+
+
+        }
+
+        //Member variables
+
+
+       
+
+
+
+
+
+
+
+
+    }
+}
